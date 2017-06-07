e60e90b1423d1690afe15506c919416d8ef25513
ThakurKafley <tkafley1@student.gsu.edu>
2017-06-06 20:35:42 -0400

    Initial commit

---
+++ app/src/main/java/edu/gsu/httpcs/thakur2017summer/testActivity.java
@@ -0,0 +1,16 @@
+package edu.gsu.httpcs.thakur2017summer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static edu.gsu.httpcs.thakur2017summer.R.layout.activity_test;
+
+public class testActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_test);
+
+    }
+}
